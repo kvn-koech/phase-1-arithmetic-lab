eda869e3c5dddf190a58a14952f66deab42f9866
Kevin Koech <kevinkoech007@gmail.com>
2025-06-12 10:44:30 +0300

    completed the test

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+const num1 = 31, num2 = 2 ;
+const multiply = num1 * num2 ;
+console.log ('multiply')
+// random integer greater than 0 
+const random = Math.floor(Math.random() * 10) +1 ;
+console.log (random)
+// remainder equal to 4
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;   
+console.log (mod)
+// max value 20
+const max = Math.max(12,11,3,5,20)
+console.log (max)
+
